796c3aae4cde4ac116b130d3cc722c5ea42c200b
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-10-09 10:07:00 +0700

    initialize repo

---
+++ M.Hibatillah_3SIC/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M.Hibatillah_3SIC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ========== Hello World ========== */
+            /*
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            */
+
+            /* ========== Input ========== */
+            /*
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+
+            String FirstName;
+            FirstName = Console.ReadLine();
+
+            String LastName;
+            Console.Write("Type your last name: ");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + FirstName + " " + LastName);
+            Console.ReadLine();
+            */
+
+            /* ========== Biodata ========== */
+            /*
+            Console.WriteLine("BIODATA");
+
+            Console.Write("Name: ");
+            String Name = Console.ReadLine();
+
+            Console.Write("Umur: ");
+            int Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Prodi: ");
+            String Major = Console.ReadLine();
+
+            Console.Write("No. Hape: ");
+            int NumberPhone = int.Parse(Console.ReadLine());
+
+            Console.Write("Alasan masuk PCR: ");
+            String Reason = Console.ReadLine();
+
+            Console.WriteLine("\nHalo saya " + Name +
+                "\nUmur " + Age + " tahun" +
+                "\nProdi " + Major +
+                "\nNo Hape " + NumberPhone +
+                "\nAlasan masuk PCR " + Reason);
+            Console.ReadLine();
+            */
+
+            /* =========== Conditional =========== */
+            /*
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door : 1, 2, or 3: ");
+            string UserValue = Console.ReadLine();
+            string Message = " ";
+
+            if (UserValue == "1") 
+            {
+                Message = "You won a new car";
+            } 
+            else if (UserValue == "2") 
+            {
+                Message = "You won a new boat";
+            } 
+            else if (UserValue == "3") 
+            {
+                Message = "You won a new cat";
+            } 
+            else 
+            {
+                Message = "Sorry we didnt understand ";
+                Message = Message + "You Lost";
+            }
+            Console.WriteLine(Message);
+            Console.ReadLine();
+            */
+
+            /* =========== Looping =========== */
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            */
+
+            /* =========== Odd & Even =========== */
+            Console.Write("Type a number: ");
+            int num = int.Parse(Console.ReadLine());
+            String desc = "";
+
+            if (num < 0)
+            {
+                desc = num + " is negative";
+            } 
+            else
+            {
+                if (num % 2 == 0)
+                {
+                    desc = num + "is Even";
+                }
+                else
+                {
+                    desc = num + " is Odd";
+                }
+            }
+            Console.WriteLine(desc);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ M.Hibatillah_3SIC/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("M.Hibatillah_3SIC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("M.Hibatillah_3SIC")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5505c741-f16d-47e8-a1de-f225e58973a3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
